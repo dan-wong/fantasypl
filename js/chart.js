021cb675feefee0ecb3662435685a78b6d0854f0
Daniel Wong <dan_wong@outlook.com>
2018-05-02 15:41:36 +1200

    Added 2nd graph

--- js/chart.js
+++ js/chart.js
@@ -22,7 +22,36 @@ function drawChart() {
           legend: { position: 'bottom' }
         };
 
-        var chart = new google.visualization.LineChart(document.getElementById('graph-div'));
+        var chart = new google.visualization.LineChart(document.getElementById('cumulative-graph-div'));
+
+        chart.draw(data, options);
+      }
+    });
+
+    Papa.parse("https://danwong.nz/fantasypl/data/gameweek_points.csv", {
+      download: true,
+      complete: function(results) {
+        results.data.splice(-1,1);
+        var data = new google.visualization.DataTable();
+        data.addRows(results.data.length)
+        for (var i = 0; i < results.data.length; i++) {
+          if (i == 0) {
+            for (var j = 0; j < 4; j++) {
+              data.addColumn('number', results.data[i][j]);
+            }
+          } else {
+            for (var j = 0; j < 4; j++) {
+              data.setCell(i, j, results.data[i][j]);
+            }
+          }
+        }
+
+        var options = {
+          title: 'Points Through the Season',
+          legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.Histogram(document.getElementById('points-gameweek-div'));
 
         chart.draw(data, options);
       }
--- scraper.py
+++ scraper.py
@@ -8,12 +8,13 @@ CUMULATIVE_POINTS_FILENAME = "gameweek_cumulative_points.csv"
 CURRENT_GAMEWEEK = 36
 
 # Daniel, Jayden, James
-PLAYER_NAMES = ["Gameweek", "Daniel", "Jayden", "James"]
+PLAYER_NAMES = ["Daniel", "Jayden", "James"]
 PLAYER_IDS = [226252, 815677, 2229616]
 
 def getPointsForGameweek(player_id, gameweek):
     r = requests.get(FPL_URL + "entry/" + str(player_id) + "/event/" + str(gameweek) + "/picks")
     jsonResponse = r.json()
+
     return jsonResponse["entry_history"]["points"]
 
 def getCumulativePointsForGameweek(player_id, gameweek):
@@ -40,11 +41,21 @@ def transposeData(gwpoints_allplayers):
     gw_points.append(PLAYER_NAMES)
     for gw in range(0, CURRENT_GAMEWEEK):
         temp_list = []
-        for player in range(0, 4):
+        for player in range(0, 3):
+            temp_list.append(gwpoints_allplayers[player - 1][gw])
+        gw_points.append(temp_list)
+    return gw_points
+
+def transposeDataWithGameweek(gwpoints_allplayers):
+    gw_points = []
+    gw_points.append("Gameweek")
+    gw_points.append(PLAYER_NAMES)
+    for gw in range(0, CURRENT_GAMEWEEK):
+        temp_list = []
+        for player in range(0, 3):
             if (player == 0):
                 temp_list.append(gw + 1)
-            else:
-                temp_list.append(gwpoints_allplayers[player - 1][gw])
+            temp_list.append(gwpoints_allplayers[player - 1][gw])
         gw_points.append(temp_list)
     return gw_points
 
@@ -60,7 +71,7 @@ print("Scraped all data")
 
 with open("data/" + CUMULATIVE_POINTS_FILENAME, "w") as output:
     writer = csv.writer(output, lineterminator='\n')
-    writer.writerows(transposeData(cumulative_points_allplayers))
+    writer.writerows(transposeDataWithGameweek(cumulative_points_allplayers))
 
 with open("data/" + POINTS_FILENAME, "w") as output:
     writer = csv.writer(output, lineterminator='\n')
